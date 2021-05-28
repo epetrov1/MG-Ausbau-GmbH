03ddca437a2b653beb0be0e37439ac4d945c1a4e
Evgeni Petrov <offerlesson@gmail.com>
2021-05-28 09:41:52 +0300

    Protect for Product

--- drybuild/settings.py
+++ drybuild/settings.py
@@ -35,11 +35,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '%1*_)-##b+ejmb9z^86)3j0kc-!7g#ilxypo9zdf(lh^22p)ok'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
 
 ALLOWED_HOSTS = []
 
