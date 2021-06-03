67ef873d7827f4304403903b24a58c98f5a0096f
Evgeni Petrov <offerlesson@gmail.com>
2021-06-03 08:46:31 +0300

    Project ready 90% But deploy

--- drybuild/settings.py
+++ drybuild/settings.py
@@ -122,12 +122,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 #Sending Emails 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'server.superhosting.bg'
-EMAIL_PORT = '25'
-EMAIL_HOST_USER = 'office@mgausbaugmbh.de'
-EMAIL_HOST_PASSWORD = 'gipskarton1234'
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 #EMAIL_USE_TLS = True
-EMAIL_USE_SSL = '465'
+EMAIL_USE_SSL = ''
 
 
 # Internationalization
